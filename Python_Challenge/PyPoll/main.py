a7b901535044b6a078c714898031f50bd7b92404
Alex Blakemore <alexblakemore@Alexs-MBP.lan>
2020-09-29 17:54:45 -0700

    updating script

--- Python_Challenge/PyPoll/main.py
+++ Python_Challenge/PyPoll/main.py
@@ -22,12 +22,12 @@ with open(election_dataCSV) as csv_file:
         number_votes = number_votes + 1
 
     # Name the Canditates
-    candidates = line[2]
+    candidate = line[2]
 
     # Tally the votes
     if candidate in candidates:
         candidate_index = candidates.index(candidate)
-        vote_count[candidate_index] = vote_counts[candidate_index] + 1
+        vote_counts[candidate_index] = vote_counts[candidate_index] + 1
 
     else:
         candidates.append(candidate)
@@ -38,10 +38,10 @@ percentages = []
 max_votes = vote_counts[0]
 max_index = 0
 
-# Determine the winner
+# Determine the winner 
 for count in range(len(candidates)):
-    vote_percentage - vote_counts[count]/number_votes *100
-    percentages.apennd(vote_percentage)
+    vote_percentage = vote_counts[count]/number_votes * 100
+    percentages.append(vote_percentage)
     if vote_counts[count] > max_votes:
         max_votes = vote_counts[count]
         print(max_votes)
@@ -54,15 +54,25 @@ print(f'Election Results')
 print(f'-------------------------')
 print(f'Total Votes: {number_votes}')
 print(f'-------------------------')
-for cocunt in range(len(candidates)):
-    print(f'{candaites[count]: {percentages[count]}% ({vote_counts[count]}))
+for count in range(len(candidates)):
+    print(f'{candidates[count]}: {percentages[count]}% ({vote_counts[count]})')
 print(f'-------------------------')
 print(f'Winner: {winner}')
 
-# Export
-output_file = election_dataCSV[0:-4]
-write_election_dataCSV = f'{output_file}pypoll_results.txt'
-filewriter = open(write_election_dataCSV, mode = "w")
+# # Export
+# output_file = election_dataCSV[0:-4]
+# write_election_dataCSV = f'{output_file}pypoll_results.txt'
+# filewriter = open(write_election_dataCSV, mode = "w")
+
+# # Text File
+# filewriter.write('Election Results')
+# frilewriter.write('-------------------------')
+# filewriter.write('Total Votes: {number_votes}')
+# filewriter.write('-------------------------')
+# for cocunt in range(len(candidates)):
+#     filewriter.write('{candidates[count]}: {percentages[count]}% ({vote_counts[count]})')
+# filewriter.write('-------------------------')
+# filewriter.write('Winner: {winner}')
 
 
             
